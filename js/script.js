510ad47bce2e16db3bf5922749756de555567d39
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-01-25 09:03:32 -0800

    initial commit of tic tac toe game's html, css, and js

---
+++ js/script.js
@@ -0,0 +1,104 @@
+// console.log("it works")
+// var myHeading = document.querySelector("h1")
+// var myList = document.querySelector("ul")
+// function reverseText() {
+//   if (myHeading.innerText = "Example Page") {
+//     myHeading.innerText = "Page Example"
+//   }
+//   else if (myHeading.innerText = "Page Example") {
+//     myHeading.innerText = "Example Page"
+//   }
+// }
+// myHeading.addEventListener('click', reverseText)
+
+// document.getElementById("myBtn").addEventListener("click", function(){
+//     this.style.backgroundColor = "red";
+// });
+
+// if (document.getElementById('gameBox1').innerText === 'X') {
+// document.getElementById('gameBox1').innerText = 'O'}
+
+alert('js is working')
+
+function changeBox1() {
+  if (document.getElementById('gameBox1').innerText === 'X') {
+    document.getElementById('gameBox1').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox1').innerText === 'O') {
+    document.getElementById('gameBox1').innerText = 'X'
+  }
+}
+function changeBox2() {
+  if (document.getElementById('gameBox2').innerText === 'X') {
+    document.getElementById('gameBox2').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox2').innerText === 'O') {
+    document.getElementById('gameBox2').innerText = 'X'
+  }
+}
+function changeBox3() {
+  if (document.getElementById('gameBox3').innerText === 'X') {
+    document.getElementById('gameBox3').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox3').innerText === 'O') {
+    document.getElementById('gameBox3').innerText = 'X'
+  }
+}
+function changeBox4() {
+  if (document.getElementById('gameBox4').innerText === 'X') {
+    document.getElementById('gameBox4').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox4').innerText === 'O') {
+    document.getElementById('gameBox4').innerText = 'X'
+  }
+}
+function changeBox5() {
+  if (document.getElementById('gameBox5').innerText === 'X') {
+    document.getElementById('gameBox5').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox5').innerText === 'O') {
+    document.getElementById('gameBox5').innerText = 'X'
+  }
+}
+function changeBox6() {
+  if (document.getElementById('gameBox6').innerText === 'X') {
+    document.getElementById('gameBox6').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox6').innerText === 'O') {
+    document.getElementById('gameBox6').innerText = 'X'
+  }
+}
+
+function changeBox7() {
+  if (document.getElementById('gameBox7').innerText === 'X') {
+    document.getElementById('gameBox7').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox7').innerText === 'O') {
+    document.getElementById('gameBox7').innerText = 'X'
+  }
+}
+function changeBox8() {
+  if (document.getElementById('gameBox8').innerText === 'X') {
+    document.getElementById('gameBox8').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox8').innerText === 'O') {
+    document.getElementById('gameBox8').innerText = 'X'
+  }
+}
+function changeBox9() {
+  if (document.getElementById('gameBox9').innerText === 'X') {
+    document.getElementById('gameBox9').innerText = 'O'
+  }
+  else if (document.getElementById('gameBox9').innerText === 'O') {
+    document.getElementById('gameBox9').innerText = 'X'
+  }
+}
+document.getElementById('gameBox1').addEventListener('click', changeBox1)
+document.getElementById('gameBox2').addEventListener('click', changeBox2)
+document.getElementById('gameBox3').addEventListener('click', changeBox3)
+document.getElementById('gameBox4').addEventListener('click', changeBox4)
+document.getElementById('gameBox5').addEventListener('click', changeBox5)
+document.getElementById('gameBox6').addEventListener('click', changeBox6)
+document.getElementById('gameBox7').addEventListener('click', changeBox7)
+document.getElementById('gameBox8').addEventListener('click', changeBox8)
+document.getElementById('gameBox9').addEventListener('click', changeBox9)
